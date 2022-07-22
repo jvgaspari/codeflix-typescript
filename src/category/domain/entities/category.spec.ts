7e4efbbe94b01e1686dd5611cb9335d767b0d84e
jose victor gaspari <josevictorgaspari@gmail.com>
2022-07-22 00:36:55 -0300

    feat: category test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,24 @@ import { Category } from './category';
 
 describe("Categoty Tests", () => {
     test('constructor of category', () => {
-        const category = new Category('Movie');
-        expect(category.name).toBe('Movie');
+        //Triple AAA - Arrange Act Assert
+
+        //Arrange
+        const props = {
+            name: 'Movie', 
+            description: 'some description', 
+            is_active: true, 
+            created_at: new Date(),
+        };
+
+        //Act
+        const category = new Category(props);
+
+        //Assert
+        expect(category.props).toStrictEqual(props);
+        // expect(category.name).toBe('Movie');
+        // expect(category.description).toBe('some description');
+        // expect(category.is_active).toBeTruthy();
+        // expect(category.created_at).toBe(props.created_at);
     }); 
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,10 +1,29 @@
+export type CategoryProperties = {
+    name: string; 
+    description?: string; 
+    is_active?: boolean; 
+    created_at?: Date;
+}
+
 export class Category {
-    constructor(
-        public name: string, 
-        public description?: string, 
-        public is_active?: boolean, 
-        public created_at?: Date
-    ) {}
+    constructor(public readonly props: CategoryProperties) {}
+
+    get name(){
+        return this.props.name;
+    }
+
+    get description(){
+        return this.props.description;
+    }
+
+    get is_active(){
+        return this.props.is_active;
+    }
+
+    get created_at(){
+        return this.props.created_at;
+    }
+    
 }
 
 //TDD - Kent Beck
